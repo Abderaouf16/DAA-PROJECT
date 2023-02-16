d2a3ac70ad94a03e57de0978a4aff42682642c3d
Abderaouf16 <abderaoufa88@gmail.comm>
2023-02-16 01:58:11 +0100

    update the project page

---
+++ DAA-project/src/components/ProjectsPage/ProjectsCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function ProjectsCard() {
+  
+    return(
+
+        <div className="w-[ 700px] h-[700px] bg-orange-700">
+            <div className="">
+                <div className="img">
+                    <img src="" alt="" />
+                </div>
+                <div className="info">
+                       <h2> project name</h2>
+                       <p> info</p>
+                       <p> the year and semester</p>
+                </div>
+            </div>
+        </div>
+    )
+    
+    
+  
+}
+
--- DAA-project/src/pages/Projects.jsx
+++ DAA-project/src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Footer from '../components/Footer/Footer'
 import home1 from '../components/CSS/Projects1.css'
 import { FaSearch } from "react-icons/fa"; 
+import ProjectsCard from '../components/ProjectsPage/ProjectsCard'
 
 
 export default function Projects() {
@@ -11,29 +12,24 @@ export default function Projects() {
   
   return(
   <>
-
-  <div className="father-container">
-    <div className="big-container ">
-      <div className="big-title">
-        <h1>Search and Browse projects of your taste</h1>
-      </div>
-      <div className="search_Container">
-        
-        <form action="" className='search_Container2'>
-      <input type="search" id="search-input" placeholder="Search..." className="search1" />
-      <button type="submit" aria-label="Submit search" className=' '>
-      <FaSearch/>
-      
-      </button>
-    </form>
-      </div>
-      <div className="projects">
-
-      </div>
+<div className=" flex justify-center ">
+<div className=' flex flex-col bg-white w-11/12 h-screen rounded-3xl  items-center  '>
+        <div className='w-full flex justify-center items-center sm:flex-row flex-col mt-4   '>
+         <h2 className='big-title font-bold text-3xl text-black text-left items-center  '> Search and browse projects of your taste </h2>
+        </div>
+        <div className=" w-5/12 h-[48px] rounded-3xl bg-black m-5 flex  justify-end">
+          <form action="" className='flex  w-11/12  '>
+            <input type="search" placeholder='search..' className='bg-transparent w-full  text-white outline-none text-xl  ' />
+            <button className='mr-2'> <FaSearch className='   text-xl  '/> </button>
+          </form>
+        </div>
+        <div className="flex flex-wrap sm:justify-start justify-center gap-8 text-black" >
+            {[1,2,3,4,5,6,7,8,9].map((song,i) =>(
+                <h1>iiii</h1>
+            ))}
+        </div>
+         </div>
     </div>
-  </div>
-
-    
     
     
   </>
